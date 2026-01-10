6d9eec311c1121d9e64ead9994d7daccd6784db8
RomanSulin <tothetwinshouse@gmail.com>
2026-01-10 18:41:59 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const cursor = document.getElementById('custom-cursor');
 
-// Двигаем курсор за мышью
 document.addEventListener('mousemove', e => {
   cursor.style.top = e.clientY + 'px';
   cursor.style.left = e.clientX + 'px';
 });
 
-// Включение эффекта при наведении на интерактивные элементы
 const hoverElements = document.querySelectorAll('a, button');
 
 hoverElements.forEach(el => {
